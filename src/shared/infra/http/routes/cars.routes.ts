5a2af00f741dc45dfbf092c4e21077353df6afec
Eduardo Braz <eduardobrazsilva45@gmail.com>
2022-04-28 12:12:04 -0300

    Problem database docker

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -2,9 +2,17 @@ import { Router } from 'express';
 
 import { CreateCarsController } from '@modules/cars/useCases/createCars/CreateCarsController';
 
+import { ensureAdmin } from '../middlewares/ensureAdmin';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+
 const carsRoutes = Router();
 
 const createCarsController = new CreateCarsController();
 
-carsRoutes.post('/', createCarsController.handle);
+carsRoutes.post(
+    '/',
+    ensureAuthenticated,
+    ensureAdmin,
+    createCarsController.handle
+);
 export { carsRoutes };
