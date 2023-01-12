fb96f216fc4f67f835a57fed07e443f3560a5615
Eduardo Braz <eduardobrazsilva45@gmail.com>
2023-01-12 19:00:22 -0300

    Correction refresh token

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
 import auth from '@config/auth';
-import { UsersTokenRepository } from '@modules/accounts/infra/typeorm/repositories/UsersTokenRepository';
 import { AppError } from '@shared/errors/AppError';
 
 interface IPayLoad {
@@ -11,11 +10,10 @@ interface IPayLoad {
 
 export async function ensureAuthenticated(
     request: Request,
-    respose: Response,
+    response: Response,
     next: NextFunction
 ) {
     const authHeader = request.headers.authorization;
-    const userTokensRepository = new UsersTokenRepository();
 
     if (!authHeader) {
         throw new AppError('Token missing', 401);
@@ -24,19 +22,7 @@ export async function ensureAuthenticated(
     const [, token] = authHeader.split(' ');
 
     try {
-        const { sub: user_id } = verify(
-            token,
-            auth.secret_refresh_token
-        ) as IPayLoad;
-
-        const user = await userTokensRepository.findByUserIdAndRefreshToken(
-            user_id,
-            token
-        );
-
-        if (!user) {
-            throw new AppError('User does not exists!', 401);
-        }
+        const { sub: user_id } = verify(token, auth.secret_token) as IPayLoad;
 
         request.user = {
             id: user_id,
